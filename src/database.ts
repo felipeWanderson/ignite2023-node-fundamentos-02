c749b2267a0616b589da3b02e81a961fb6dbcbe6
Felipe Leal <felipewanderson2020@gmail.com>
2023-12-17 22:20:16 -0300

    feat: create config migrations

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,14 @@
-import { knex as setupKnex } from 'knex'
-
-export const knex = setupKnex({
+import { knex as setupKnex, Knex } from 'knex'
+export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './tmp/app.db',
+    filename: './db/app.db',
+  },
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
   },
   useNullAsDefault: true,
-})
+}
+
+export const knex = setupKnex(config)
